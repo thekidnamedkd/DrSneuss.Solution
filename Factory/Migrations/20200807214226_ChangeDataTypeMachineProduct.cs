c5495d61e5a1e1447b54483642e39aa227fba5ea
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-07 14:46:04 -0700

    fix error with datatype for MachineProduct. update database with migration.

---
+++ Factory/Migrations/20200807214226_ChangeDataTypeMachineProduct.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class ChangeDataTypeMachineProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MachineProduct",
+                table: "Machines",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MachineProduct",
+                table: "Machines",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
